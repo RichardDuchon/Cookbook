7acb3b07cd3c2003ce23afaa046342fba77d9176
RichardDuchon <RichardDuch@seznam.cz>
2020-09-19 21:21:59 +0200

    Add new project

--- Cookbook.DAL/Class1.cs
+++ Cookbook.DAL/Class1.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace Cookbook.DAL
 {
+
 
     public class CookbookDbContext : DbContext
     {
@@ -11,6 +13,28 @@ namespace Cookbook.DAL
         public DbSet<AuthorsEntity> Author { get; set; }
         public DbSet<ReviewEntity> Review { get; set; }
         public DbSet<RecipeEntity> Recipe { get; set; }
+
+        public CookbookDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<CookbookDbContext>
+        {
+            public CookbookDbContext CreateDbContext(string[] args)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<CookbookDbContext>();
+                optionsBuilder.UseMySQL(@"server=remotemysql.com;user id=wkL3rf6tSa; Password=QY2494NM6P; database=wkL3rf6tSa");
+
+                return new CookbookDbContext(optionsBuilder.Options);
+            }
+        }
 
     }
 
